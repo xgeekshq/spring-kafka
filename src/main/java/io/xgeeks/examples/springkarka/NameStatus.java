7b1d08a88be58d46b658bfbfb56bb19015a8e961
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-30 20:22:45 +0100

    updates name status

---
+++ src/main/java/io/xgeeks/examples/springkarka/NameStatus.java
@@ -0,0 +1,36 @@
+package io.xgeeks.examples.springkarka;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NameStatus {
+
+    private final String name;
+
+    private final int value;
+
+    public NameStatus(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "NameStatus{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+
+    public static NameStatus of(Map.Entry<String, AtomicInteger> entry) {
+        return new NameStatus(entry.getKey(), entry.getValue().intValue());
+    }
+}
