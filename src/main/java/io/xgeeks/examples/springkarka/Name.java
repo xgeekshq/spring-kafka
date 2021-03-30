c181f0dcfaec52a13e7d6fe919e601968afb9e6b
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-30 20:35:58 +0100

    defines name as mutuable class

--- src/main/java/io/xgeeks/examples/springkarka/Name.java
+++ src/main/java/io/xgeeks/examples/springkarka/Name.java
@@ -1,15 +1,21 @@
 package io.xgeeks.examples.springkarka;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
 public class Name {
 
-    private String name;
+    private final String name;
+
+    @JsonCreator
+    public Name(@JsonProperty("name") String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
@@ -17,5 +23,22 @@ public class Name {
         return "Name{" +
                 "name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
