7152f1c7af39d5cc91999e415466ce565c0ec916
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-30 11:41:24 +0100

    creates consumer class

--- src/main/java/io/xgeeks/examples/springkarka/Consumer.java
+++ src/main/java/io/xgeeks/examples/springkarka/Consumer.java
@@ -1,2 +1,13 @@
-package io.xgeeks.examples.springkarka;public class Consumer {
+package io.xgeeks.examples.springkarka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @KafkaListener(id = "myId", topics = "topic1")
+    public void listen(String in) {
+        System.out.println(in);
+    }
 }
