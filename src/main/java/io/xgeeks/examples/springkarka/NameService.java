ef29fe6b05bbf9af8b604e6b9367c68e709692f4
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-30 20:23:13 +0100

    updates name service

--- src/main/java/io/xgeeks/examples/springkarka/NameService.java
+++ src/main/java/io/xgeeks/examples/springkarka/NameService.java
@@ -1,4 +1,27 @@
 package io.xgeeks.examples.springkarka;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class NameService {
+
+    private final NameCounter counter;
+
+    public NameService(NameCounter counter) {
+        this.counter = counter;
+    }
+
+    public List<NameStatus> findAll() {
+        return counter.getValues()
+                .map(NameStatus::of)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public Optional<NameStatus> findByName(String name) {
+        return Optional.empty();
+    }
 }
