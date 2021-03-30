c1322343b168ec7874c68d71ac967f6c3c8d48e2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-30 11:33:21 +0100

    creates initial project

---
+++ src/main/java/io/xgeeks/examples/springkarka/SpringKarkaApplication.java
@@ -0,0 +1,13 @@
+package io.xgeeks.examples.springkarka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKarkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKarkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/xgeeks/examples/springkarka/SpringKarkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.xgeeks.examples.springkarka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKarkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
